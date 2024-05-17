826f397b1ec41382b04ba9b40c500c1184c0205e
pmbittner <paul.bittner@uni-ulm.de>
2024-05-17 10:31:38 +0200

    doom: disable centaur-tabs in dired

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -320,6 +320,7 @@
   ;; (setq centaur-tabs-style "wave")
   ;; (setq centaur-tabs-set-bar 'under)
   ;; (setq x-underline-at-descent-line t)
+  (add-hook 'dired-mode-hook 'centaur-tabs-local-mode)
   (setq centaur-tabs-excluded-prefixes
         (append centaur-tabs-excluded-prefixes
                '("*Async-native-compile-log"
