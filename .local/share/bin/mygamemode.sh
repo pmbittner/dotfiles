615f0a5a42919ae13a3c097a5a5455f2c5bd589b
pmbittner <paul.bittner@uni-ulm.de>
2024-05-28 10:42:20 +0200

    hypr: some tweaks to mygamemode

--- .local/share/bin/mygamemode.sh
+++ .local/share/bin/mygamemode.sh
@@ -21,11 +21,26 @@ if [ $HYPRGAMEMODE = 1 ]; then
         keyword general:gaps_out 0;\
         keyword general:gaps_in 0;\
         keyword decoration:drop_shadow 0;\
+		keyword misc:vfr:enabled 0;\
+		keyword decoration:active_opacity 1;\
+		keyword decoration:inactive_opacity 1;\
+		keyword decoration:fullscreen_opacity 1;\
         keyword decoration:rounding 0"
 	exit
 else
 	hyprctl reload
 fi
+
+## This disables transparency for the current window.
+# hyprctl dispatch toggleopaque
+## This sets properties for all currently open windows.
+## The set properties will turn of any transparency stuff.
+# hyprctl clients -j | jq -r ".[].address" | xargs -I {} hyprctl setprop address:{} forceopaque 1 lock
+# hyprctl clients -j | jq -r ".[].address" | xargs -I {} hyprctl setprop address:{} forceopaqueoverriden 1 lock
+# hyprctl clients -j | jq -r ".[].address" | xargs -I {} hyprctl setprop address:{} alphaoverride 1 lock
+# hyprctl clients -j | jq -r ".[].address" | xargs -I {} hyprctl setprop address:{} alpha 1 lock
+# hyprctl clients -j | jq -r ".[].address" | xargs -I {} hyprctl setprop address:{} alphainactiveoverride 1 lock
+# hyprctl clients -j | jq -r ".[].address" | xargs -I {} hyprctl setprop address:{} alphainactive 1 lock
 
 # I removed the deactivation of blur to keep blur.
 # The problem was that removing blur made all windows a lot
