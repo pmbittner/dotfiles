9d58356ac78253688b1f62a89d268f5cc3c6b45b
pmbittner <paul.bittner@uni-ulm.de>
2024-09-09 15:58:41 +0200

    doom: progress on using everforest theme

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -16,6 +16,7 @@
 (package! evil-snipe :disable t)
 ;; (package! everforest
 ;;   :recipe (:repo "https://git.sr.ht/~theorytoe/everforest-theme"))
+;;   ;; :recipe (:repo "https://github.com/Theory-of-Everything/everforest-emacs.git"))
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
