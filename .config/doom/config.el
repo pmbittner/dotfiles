79147b3a9b5264b6dcef88f1997f63f22db55453
pmbittner <paul.bittner@uni-ulm.de>
2024-09-25 16:36:09 +0200

    doom: fix agda mode not loaded for .agda files

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -466,6 +466,7 @@
   (add-hook! 'agda2-mode-hook
     (setq-local evil-shift-width 2)
     (activate-input-method "Agda"))
+  (prependq! auto-mode-alist '(("\\.agda\\'" . agda2-mode)))
   (prependq! auto-mode-alist '(("\\.lagda.md\\'" . agda2-mode)))
   ;; Make evil repeat (pressing .) ignore agda2-load.
   (evil-add-command-properties 'agda2-load :repeat 'ignore)
