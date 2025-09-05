66005b4acb616e9fc04224f4d6b49639aa16ffb5
pmbittner <paul.bittner@uni-ulm.de>
2025-09-05 14:57:51 +0200

    nixos: inkscape, ccrypt

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -118,6 +118,8 @@ in
       graphviz
       powertop
       libreoffice
+      inkscape
+      ccrypt
       # packages from unstable channel
       nixos-unstable.signal-desktop
       nixos-unstable.zoom-us
