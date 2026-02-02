c3b6672c2e23180318556d76351ff10978d12231
pmbittner <paul.bittner@uni-ulm.de>
2026-02-02 17:41:17 +0100

    doom(centaur-tabs): underlines

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -464,8 +464,8 @@
 
 (after! centaur-tabs
   ;; (setq centaur-tabs-style "wave")
-  (setq centaur-tabs-set-bar nil)
-  ;; (setq x-underline-at-descent-line t)
+  (setq centaur-tabs-set-bar 'under)
+  (setq x-underline-at-descent-line t)
   (add-hook 'dired-mode-hook 'centaur-tabs-local-mode)
   ;; (setq centaur-tabs-excluded-prefixes
   ;;       (append centaur-tabs-excluded-prefixes '(
