3880150fa0a8b5f5881c80f1244700d3c470ef19
pmbittner <paul.bittner@salzgitter-digital.de>
2026-08-13 15:00:22 +0200

    doom: update theme config to newest doom version

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -250,11 +250,12 @@
   )
 
 (use-package doom-themes
-  ;; :ensure t
-  :config
+  :ensure t
+  :custom
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  :config
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
   ;; Enable custom neotree theme (all-the-icons must be installed!)
